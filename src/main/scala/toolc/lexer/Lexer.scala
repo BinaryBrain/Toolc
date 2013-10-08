3e8bd53f4686eae946ff54f91948e0490c02da0d
Andy Roulin <andy.roulin@epfl.ch>
2013-10-08 20:50:21 +0200

    Adds IntLit to Lexer but had to change ch to String

--- src/main/scala/toolc/lexer/Lexer.scala
+++ src/main/scala/toolc/lexer/Lexer.scala
@@ -11,19 +11,18 @@ object Lexer extends Pipeline[File, Iterator[Token]] {
   def run(ctx: Context)(f: File): Iterator[Token] = {
     val source = Source.fromFile(f)
     import ctx.reporter._
-
-    // Complete this file
-
+    
     new Iterator[Token] {
       var token: Token = null
-      var ch = source.next
+      var ch = source.next.toString
       var pos = source.pos
 
       def nextCh = {
-        ch = source.next
+        ch = source.next.toString
       }
       
       def hasNext = {
+        // ask what to do if file is empty
         token == null || token.kind != EOF
       }
 
@@ -35,72 +34,96 @@ object Lexer extends Pipeline[File, Iterator[Token]] {
         }
         
         else {
+          
+          val IntegerStart = "([1-9])".r
+          val IntegerMiddle = "([0-9])".r
+          
           token = ch match {
             
-            case ' '  => nextCh; next
-            case '\n' => nextCh; next
+            case " "  => nextCh; next
+            case "\n" => nextCh; next
             
             
-            case ':' => nextCh; new Token(COLON)
-            case ';' => nextCh; new Token(SEMICOLON)
-            case '.' => nextCh; new Token(DOT)
-            case ',' => nextCh; new Token(COMMA)
-            case '!' => nextCh; new Token(BANG)
-            case '(' => nextCh; new Token(LPAREN)
-            case ')' => nextCh; new Token(RPAREN)
-            case '[' => nextCh; new Token(LBRACKET)
-            case ']' => nextCh; new Token(RBRACKET)
-            case '{' => nextCh; new Token(LBRACE)
-            case '}' => nextCh; new Token(RBRACE)
-            case '<' => nextCh; new Token(LESSTHAN)
-            case '+' => nextCh; new Token(PLUS)
-            case '-' => nextCh; new Token(MINUS)
-            case '*' => nextCh; new Token(TIMES)
+            case ":" => nextCh; new Token(COLON)
+            case ";" => nextCh; new Token(SEMICOLON)
+            case "." => nextCh; new Token(DOT)
+            case "," => nextCh; new Token(COMMA)
+            case "!" => nextCh; new Token(BANG)
+            case "(" => nextCh; new Token(LPAREN)
+            case ")" => nextCh; new Token(RPAREN)
+            case "[" => nextCh; new Token(LBRACKET)
+            case "]" => nextCh; new Token(RBRACKET)
+            case "{" => nextCh; new Token(LBRACE)
+            case "}" => nextCh; new Token(RBRACE)
+            case "<" => nextCh; new Token(LESSTHAN)
+            case "+" => nextCh; new Token(PLUS)
+            case "-" => nextCh; new Token(MINUS)
+            case "*" => nextCh; new Token(TIMES)
             
-            case '&' => nextCh;
-            			if(ch == '&') {
+            case IntegerStart(x)  => 
+              nextCh
+              var integer = x.toInt
+              var cont = true
+              while(cont) {
+                ch match {
+                  case IntegerMiddle(y) =>
+                		  integer = integer * 10 + y.toInt
+                		  nextCh
+                  case _ =>
+                		  cont = false
+                }
+              }
+              
+              new INTLIT(integer)
+            
+            case "&" => nextCh;
+            			if(ch == "&") {
             			  nextCh
             			  new Token(AND)
             			} else {
             			  new Token(BAD)
             			}
             
-            case '|' => nextCh;
-            			if(ch == '|') {
+            case "|" => nextCh;
+            			if(ch == "|") {
             			  nextCh
             			  new Token(OR)
             			} else {
             			  new Token(BAD)
             			}
             
-            case '=' => nextCh;
-            			if(ch == '=') {
+            case "=" => nextCh;
+            			if(ch == "=") {
             			  nextCh
             			  new Token(EQUALS)
             			} else {
             			  new Token(EQSIGN)
             			}			
             
-            case '/' => nextCh;
-            			if(ch == '/') {
-            			  while(ch != '\n' && source.hasNext) nextCh;
+            case "/" => nextCh;
+            
+            			// Single-line comment
+            			if(ch == "/") {
+            			  while(ch != "\n" && source.hasNext) nextCh;
             			  if(source.hasNext) nextCh
-            			  next }
+            			  next 
+            			}
             			
-            			else if(ch == '*') {
+            			/* Block Comment */
+            			else if(ch == "*") {
             			  nextCh
             			  var cont = true
             			  while(cont) {
-            				  while(ch != '*' && source.hasNext) 
-            				    nextCh
+            				  while(ch != "*" && source.hasNext) nextCh
             				  if(source.hasNext) nextCh
-            				  if(ch == '/' || !source.hasNext) {
+            				  if(ch == "/" || !source.hasNext) {
             				    cont = false
             				  }
             				  if(source.hasNext) nextCh
             			  }
             			  next }
             			
+            			// Division sign
             			else {
             			  new Token(DIV)
             			}
