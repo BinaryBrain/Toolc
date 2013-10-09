54323a887cb9ecee980ccf360ec0dfa872ed1892
Andy Roulin <andy.roulin@epfl.ch>
2013-10-09 11:12:51 +0200

    Completes Lexer

--- src/main/scala/toolc/lexer/Lexer.scala
+++ src/main/scala/toolc/lexer/Lexer.scala
@@ -14,11 +14,11 @@ object Lexer extends Pipeline[File, Iterator[Token]] {
     
     new Iterator[Token] {
       var token: Token = null
-      var ch = source.next.toString
+      var ch = source.next
       var pos = source.pos
 
       def nextCh = {
-        ch = source.next.toString
+        ch = source.next
       }
       
       def hasNext = {
@@ -35,39 +35,35 @@ object Lexer extends Pipeline[File, Iterator[Token]] {
         
         else {
           
-          val IntegerStart = "([1-9])".r
-          val IntegerMiddle = "([0-9])".r
-          
           token = ch match {
             
-            case " "  => nextCh; next
-            case "\n" => nextCh; next
+            case c if c.toString.matches("\\s")  => nextCh; next
             
             
-            case ":" => nextCh; new Token(COLON)
-            case ";" => nextCh; new Token(SEMICOLON)
-            case "." => nextCh; new Token(DOT)
-            case "," => nextCh; new Token(COMMA)
-            case "!" => nextCh; new Token(BANG)
-            case "(" => nextCh; new Token(LPAREN)
-            case ")" => nextCh; new Token(RPAREN)
-            case "[" => nextCh; new Token(LBRACKET)
-            case "]" => nextCh; new Token(RBRACKET)
-            case "{" => nextCh; new Token(LBRACE)
-            case "}" => nextCh; new Token(RBRACE)
-            case "<" => nextCh; new Token(LESSTHAN)
-            case "+" => nextCh; new Token(PLUS)
-            case "-" => nextCh; new Token(MINUS)
-            case "*" => nextCh; new Token(TIMES)
+            case ':' => nextCh; new Token(COLON)
+            case ';' => nextCh; new Token(SEMICOLON)
+            case '.' => nextCh; new Token(DOT)
+            case ',' => nextCh; new Token(COMMA)
+            case '!' => nextCh; new Token(BANG)
+            case '(' => nextCh; new Token(LPAREN)
+            case ')' => nextCh; new Token(RPAREN)
+            case '[' => nextCh; new Token(LBRACKET)
+            case ']' => nextCh; new Token(RBRACKET)
+            case '{' => nextCh; new Token(LBRACE)
+            case '}' => nextCh; new Token(RBRACE)
+            case '<' => nextCh; new Token(LESSTHAN)
+            case '+' => nextCh; new Token(PLUS)
+            case '-' => nextCh; new Token(MINUS)
+            case '*' => nextCh; new Token(TIMES)
             
-            case IntegerStart(x)  => 
+            case x if (x.isDigit && x != '0')  => 
               nextCh
-              var integer = x.toInt
+              var integer = x.asDigit
               var cont = true
               while(cont) {
                 ch match {
-                  case IntegerMiddle(y) =>
-                		  integer = integer * 10 + y.toInt
+                  case y if y.isDigit =>
+                		  integer = integer * 10 + y.asDigit
                 		  nextCh
                   case _ =>
                 		  cont = false
@@ -75,48 +71,94 @@ object Lexer extends Pipeline[File, Iterator[Token]] {
               }
               
               new INTLIT(integer)
+              
+            case '0' => nextCh; new INTLIT(0)
             
-            case "&" => nextCh;
-            			if(ch == "&") {
+            case '&' => nextCh;
+            			if(ch == '&') {
             			  nextCh
             			  new Token(AND)
             			} else {
             			  new Token(BAD)
             			}
             
-            case "|" => nextCh;
-            			if(ch == "|") {
+            case '|' => nextCh;
+            			if(ch == '|') {
             			  nextCh
             			  new Token(OR)
             			} else {
             			  new Token(BAD)
             			}
             
-            case "=" => nextCh;
-            			if(ch == "=") {
+            case '=' => nextCh;
+            			if(ch == '=') {
             			  nextCh
             			  new Token(EQUALS)
             			} else {
             			  new Token(EQSIGN)
-            			}			
+            			}
+            			
+            case c if c.isLetter => 
+              var id = c.toString
+              nextCh
+              while(ch.isLetterOrDigit || ch == '_') {
+                id = id + ch
+                nextCh
+              }
+              
+            id match {
+              case "object" => new Token(OBJECT)
+              case "class" => new Token(CLASS)
+              case "def" => new Token(DEF)
+              case "var" => new Token(VAR)
+              case "Unit" => new Token(UNIT)
+              case "main" => new Token(MAIN)
+              case "String" => new Token(STRING)
+              case "extends" => new Token(EXTENDS)
+              case "Int" => new Token(INT)
+              case "Bool" => new Token(BOOLEAN)
+              case "while" => new Token(WHILE)
+              case "if" => new Token(IF)
+              case "else" => new Token(ELSE)
+              case "return" => new Token(RETURN)
+              case "length" => new Token(LENGTH)
+              case "true" => new Token(TRUE)
+              case "false" => new Token(FALSE)
+              case "this" => new Token(THIS)
+              case "new" => new Token(NEW)
+              case "println" => new Token(PRINTLN)
+              
+              case _ => new ID(id)
+            }
+              
+            			
+            case '"' =>
+              	nextCh
+              	var str = ""
+              	while(ch != '"') {
+              	  str = str + ch
+              	  nextCh
+              	}
+              	nextCh
+              	new STRLIT(str)
             
-            case "/" => nextCh;
+            case '/' => nextCh
             
             			// Single-line comment
-            			if(ch == "/") {
-            			  while(ch != "\n" && source.hasNext) nextCh;
+            			if(ch == '/') {
+            			  while(ch != '\n' && source.hasNext) nextCh;
             			  if(source.hasNext) nextCh
             			  next 
             			}
             			
             			/* Block Comment */
-            			else if(ch == "*") {
+            			else if(ch == '*') {
             			  nextCh
             			  var cont = true
             			  while(cont) {
-            				  while(ch != "*" && source.hasNext) nextCh
+            				  while(ch != '*' && source.hasNext) nextCh
             				  if(source.hasNext) nextCh
-            				  if(ch == "/" || !source.hasNext) {
+            				  if(ch == '/' || !source.hasNext) {
             				    cont = false
             				  }
             				  if(source.hasNext) nextCh
